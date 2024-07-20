27487024f440d7d92ac3d88f3f990b61614b75b3
Suellenon <sullen502@gmail.com>
2024-07-20 17:01:30 -0300

    alguns.arq mobiles

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+
+
+function  barra_lateral(){
+    const menuLateral= document.getElementById('versao-mobile');
+   
+    
+    const btnLateral= document.getElementById('btn-lateral');
+    
+
+    if('click'){
+    btnLateral.src="../imgs/close1.png";
+    btnLateral.style.width="20px";
+    menuLateral.classList.toggle('abrir')
+   }    
+    }
+   
+    
+
