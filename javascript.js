edc6d9a55bbbbeb8980f35a21e8808a8f0f4aac0
Suellenon <sullen502@gmail.com>
2024-07-20 17:40:19 -0300

    anim.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,37 @@
 
 
 
-function  barra_lateral(){
-    const menuLateral= document.getElementById('versao-mobile');
+// function  barra_lateral(){
+//     const menuLateral= document.getElementById('versao-mobile');
    
     
-    const btnLateral= document.getElementById('btn-lateral');
+//     const btnLateral= document.getElementById('btn-lateral');
     
 
-    if('click'){
-    btnLateral.src="../imgs/close1.png";
-    btnLateral.style.width="20px";
-    menuLateral.classList.toggle('abrir')
-   }    
-    }
+//     if('click'){
+//     btnLateral.src="../imgs/close1.png";
+//     btnLateral.style.width="20px";
+//     menuLateral.classList.toggle('abrir')
+//    }    
+//     }
    
-    
+function barra_lateral() {
+    const menuLateral = document.getElementById('versao-mobile');
+    const btnLateral = document.getElementById('btn-lateral');
+
+    // Verifica se a classe 'abrir' está presente no menuLateral
+    const isOpen = menuLateral.classList.contains('abrir');
+
+    if (!isOpen) {
+        // Se o menu lateral estiver fechado, abre
+        btnLateral.src = "../imgs/close1.png";
+        btnLateral.style.width = "20px";
+        menuLateral.classList.add('abrir');
+    } else {
+        // Se o menu lateral estiver aberto, fecha
+        btnLateral.src = "../imgs/burger-bar.png"; // Aqui você define o caminho do ícone de hambúrguer
+        btnLateral.style.width = "30px"; // Remove a largura definida (volta ao padrão)
+        menuLateral.classList.remove('abrir');
+    }
+}
 
